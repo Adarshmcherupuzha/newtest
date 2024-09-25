0b49f0132b1b915bbe588d57ff73fa99001da100
Adarshmcherupuzha <adarshmpadichal@gmail.com>
2024-09-25 15:08:44 +0530

    first commit

---
+++ src/sum.java
@@ -0,0 +1,20 @@
+public class sum {
+
+    // Method to calculate the sum of two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        sum sumCalculator = new sum();
+
+        int num1 = 5;
+        int num2 = 10;
+
+        // Calculate the sum
+        int result = sumCalculator.add(num1, num2);
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
